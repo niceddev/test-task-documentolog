38ffd823a5735bed36c4233986c2e986572e5002
naruto <niceddeveloper@gmail.com>
2023-02-23 01:08:41 +0600

    add: singleton and sql

--- taskOne/MySingleton.php
+++ taskOne/MySingleton.php
@@ -2,5 +2,25 @@
 
 class MySingleton
 {
+    private static ?MySingleton $instance = null;
+
+    public static function getInstance()
+    {
+        if (is_null(self::$instance)) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    public function __clone(): void
+    {
+        // TODO: Implement __clone() method.
+    }
+
+    public function __wakeup(): void
+    {
+        // TODO: Implement __wakeup() method.
+    }
 
 }
--- taskTwo/employees.sql
+++ taskTwo/employees.sql
@@ -1,1 +1,3 @@
-select * from employees;
+
+
+select department, count(distinct name) from employees group by department having count(distinct name) < 3
