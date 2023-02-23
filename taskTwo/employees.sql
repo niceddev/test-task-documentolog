8d3f905a68be5c68b7c5b2b3ad7753af7d8baa20
aslan <niceddeveloper@gmail.com>
2023-02-23 10:24:17 +0600

    done: task two

--- taskTwo/employees.sql
+++ taskTwo/employees.sql
@@ -1,3 +1,7 @@
+select * from employees e
+    left join employees e2 on e2.employeeid = e.chiefid
+where e.salary > e2.salary;
 
-
-select department, count(distinct name) from employees group by department having count(distinct name) < 3
+select department, count(distinct name) from employees
+group by department
+having count(distinct name) < 3;
